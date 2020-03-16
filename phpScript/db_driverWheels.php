19f279878fa521be79c6942bc920c212b7feadce
Evgenii <tenros@googlemail.com>
2020-03-16 23:02:45 +0300

    Синхронизированно содержимое
    На хостинге ранее созданы файлы с содержимым. В index.html  добавлена форма для поиска в бд данных по лекалам. Файл php содержить учетные данные для соединения с БД созданной на MySQL

---
+++ phpScript/db_driverWheels.php
@@ -0,0 +1,8 @@
+<?php 
+	const USER = 'id12939332_tenros1983';
+	const SERVER =' localhost';
+	const PASSWORD = 'driverWheels16032020 ';
+	const DB_NAME = 'id12939332_db_driverwheels';
+	echo USER; 
+	
+?>
