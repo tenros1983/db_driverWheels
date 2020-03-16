3058caaa2f1a8daf4536e6336cebd210687f1c01
Evgenii <tenros@googlemail.com>
2020-03-16 23:21:26 +0300

    Добавляем команды для получения данных из БД

--- phpScript/db_driverWheels.php
+++ phpScript/db_driverWheels.php
@@ -1,8 +1,14 @@
-<?php 
+<?php
 	const USER = 'id12939332_tenros1983';
 	const SERVER =' localhost';
 	const PASSWORD = 'driverWheels16032020 ';
 	const DB_NAME = 'id12939332_db_driverwheels';
-	echo USER; 
-	
+
+	$us_Connecting = new mysqli(SERVER, USER, PASSWORD, DB_NAME);
+
+	$us_QueryResult = $us_Connecting->query("SELECT * FROM carCatalog");
+
+
+	echo USER;
+
 ?>
