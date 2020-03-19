7ce58a87d0550aa5867e744b5138a286edbb8fcb
Evgenii <tenros@googlemail.com>
2020-03-19 23:03:25 +0300

    Добавление
    Добавлен мета тег описывающий поведение страницы во время отображения на различных устройствах

--- phpScript/db_driverWheels.php
+++ phpScript/db_driverWheels.php
@@ -1,14 +1,25 @@
 <?php
-	const USER = 'id12939332_tenros1983';
-	const SERVER =' localhost';
-	const PASSWORD = 'driverWheels16032020 ';
-	const DB_NAME = 'id12939332_db_driverwheels';
+	const USER = "id12939332_tenros1983";
+	const SERVER = "localhost";
+	const PASSWORD = "Nbvjattdty28061983";
+	const DB_NAME = "id12939332_db_driverwheels";
 
 	$us_Connecting = new mysqli(SERVER, USER, PASSWORD, DB_NAME);
+	if ($us_Connecting->connect_errno) {
+		printf("Error ->%\n", $us_Connecting->connect_error);
+		exit();
+	}
 
-	$us_QueryResult = $us_Connecting->query("SELECT * FROM carCatalog");
+ 	//$us_QueryResult = $us_Connecting->query("SELECT * FROM carCatalog");
 
+	$stmp = $us_Connecting->prepare("INSERT INTO tbl_carCatalog VALUES(?, ?)");
+	$stmp->bind_param(Null, "Audi", "A6", Null, Null);
+	$stmp->execute();
+	$stmp->close();
 
-	echo USER;
+	$us_Connecting->close();
+
+	//$us_QueryResult = $us_Connecting->query("SELECT * FROM carCatalog");
+
 
 ?>
